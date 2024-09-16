86ea63034347a917aee5b35b4f97137e960522b5
JuanPabloMerino <jpmerino@hiva.com.ar>
2024-09-16 11:08:39 -0300

    config pool connections

--- src/database.ts
+++ src/database.ts
@@ -1,22 +1,28 @@
-import { createPool } from "mysql2/promise";
+import { createPool, Pool } from "mysql2/promise";
 import "dotenv/config";
 
-async function connect() {
-  try {
-    const connection = await createPool({
-      host: process.env.DB_HOST || "localhost",
-      user: process.env.DB_USER || "root",
-      database: process.env.DB_NAME || "data_base_spa",
-      password: process.env.DB_PASS || "",
-    });
+let pool: Pool | undefined;
 
-    return connection;
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-    throw error;
+async function connect(): Promise<Pool> {
+  if (!pool) {
+    try {
+      pool = await createPool({
+        host: process.env.DB_HOST || "localhost",
+        user: process.env.DB_USER || "root",
+        database: process.env.DB_NAME || "data_base_spa",
+        password: process.env.DB_PASS || "",
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+      });
+    } catch (error) {
+      console.error("Error connecting to the database:", error);
+      throw error;
+    }
   }
+  return pool;
 }
 
-export async function getConnection() {
+export async function getConnection(): Promise<Pool> {
   return await connect();
 }
