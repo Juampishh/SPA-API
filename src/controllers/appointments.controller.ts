551d17378eae4fcd1b3a3f7fe542fefa10cd598c
JuanPabloMerino <jpmerino@hiva.com.ar>
2024-10-21 15:15:21 -0300

    fix bug

--- src/controllers/appointments.controller.ts
+++ src/controllers/appointments.controller.ts
@@ -171,9 +171,6 @@ export const createAppointment = async (
       });
     });
 
-    // Add company logo
-    doc.image("https://ibb.co/kBvfvxj", 50, 45, { width: 100 }); // Adjust the path and size as needed
-
     // Add content to the PDF
     doc.fontSize(20).text("Spa Sentirse Bien", { align: "center" });
     doc
